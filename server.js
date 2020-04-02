76a999762b41355d5d5541119ce168eae37c8706
Kittithee Thaweesuk <kittithee2540@hotmail.com>
2020-04-02 15:17:47 +0700

    Create dist

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/Covid19/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
+// Start the app by listening on the default Heroku port 8080
 app.listen(process.env.PORT || 8080);
